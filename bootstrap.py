ac165d00e2b6eec5a73bfed7c9ce8363d9f64fb9
Mark Waite <MarkWaite@yahoo.com>
2013-05-29 07:45:33 -0600

    Use default download URL if no argument is provided

--- bootstrap.py
+++ bootstrap.py
@@ -1,8 +1,13 @@
 #! /usr/bin/python
 
+import urllib2
 import sys
 
 def bootstrap(args):
+    download_url = "http://mirrors.jenkins-ci.org/war-stable-rc/latest/jenkins.war"
+    if args:
+        download_url = args[0]
+    print "Download URL is", download_url
     pass
 
 if __name__ == "__main__":

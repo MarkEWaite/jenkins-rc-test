2bf8d2d5b751a5a89263055af16c531f55b85ec8
Mark Waite <MarkWaite@yahoo.com>
2013-05-29 08:41:32 -0600

    Create download_jenkins_war function to download war file if not already cached locally
    The function is incomplete, but a working prototype of the first steps

--- bootstrap.py
+++ bootstrap.py
@@ -1,21 +1,33 @@
 #! /usr/bin/python
 
+import os
 import sys
 import time
 import urllib2
+
+def download_jenkins_war(url, destination_directory):
+    print "Downloading ", url
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    info = response.info()
+    url_last_modified = time.strptime(info["Last-Modified"], "%a, %d %b %Y %H:%M:%S %Z")
+    print "Content last modified:", url_last_modified[:6]
+    url_file_name = url.split("/")[-1]
+    print "URL file name is", url_file_name
+    destination_file = os.path.join(destination_directory, url_file_name)
+    print "Destination file name is", destination_file
+    if os.path.isfile(destination_file):
+        file_last_modified = time.ctime(os.path.getmtime(destination_file))
+    else:
+        file_last_modified = time.ctime(12 * 60 * 60) # Jan 1, 1970
+    print "Local file last modified:", file_last_modified
+    pass
 
 def bootstrap(args):
     download_url = "http://mirrors.jenkins-ci.org/war-stable-rc/latest/jenkins.war"
     if args:
         download_url = args[0]
-    print "Downloading ", download_url
-    req = urllib2.Request(download_url)
-    response = urllib2.urlopen(req)
-    info = response.info()
-    last_modified_str = info["Last-Modified"]
-    print last_modified_str
-    last_modified = time.strptime(last_modified_str, "%a, %d %b %Y %H:%M:%S %Z")
-    print "LTS RC download_url content last modified:", last_modified[:6]
+    download_jenkins_war(download_url, ".")
     pass
 
 if __name__ == "__main__":
